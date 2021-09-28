9a2f8c38994b4fb8c001d9718f04415f9b0b813c
YoungSeng <940863869@qq.com>
2021-09-28 20:21:53 +0800

    make '.csv' and kaggle rank

---
+++ TextToCSV.py
@@ -0,0 +1,23 @@
+
+# import pandas as pd
+import csv
+
+root = r'C:\HCSI\SiCheng Yang\杨思程\我的资料库\我的坚果云\课程\大数据机器学习\第一次实验\\'
+txt_path = root + 'submission.txt'
+csv_path = root + 'submission.csv'
+
+with open(csv_path, 'w+', newline='') as fh:
+    writer = csv.writer(fh)
+    # writer.writerow(['Id, Expected'])
+
+    res = []
+    with open(txt_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            res.append([x for x in line.strip().split(',')])
+            # res.append([line])
+
+    # print(res)
+    writer.writerows(res)
+
+
+
